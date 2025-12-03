b847ac59d8cc8abb1366c39545c155a9bb2eb9e5
jprowak1 <jprowak@gmail.com>
2025-12-02 20:59:38 -0500

    started writing the get_color function

--- reading_pil_img.py
+++ reading_pil_img.py
@@ -3,12 +3,29 @@ from PIL import Image
 import os
 # image is 3020 square
 CUBE_WIDTH = 1007
+# middle of each horizontal pixel
 HALF_CUBE_WIDTH = CUBE_WIDTH/2
 CUBE0_MID = HALF_CUBE_WIDTH
 CUBE1_MID = CUBE0_MID + CUBE_WIDTH
 CUBE2_MID = CUBE1_MID + CUBE_WIDTH
 cube_smpling_pts = (CUBE0_MID, CUBE1_MID, CUBE2_MID)
-# middle of each horizontal pixel
+
+def get_color (smpl):
+    RED = (255,0,0)
+    GR = (0, 255,0)
+    BL = (0,0,255)
+    OR = (200,80,80)
+    YE = (0, 255,255)
+    WH = (255,255,255)
+    red_int, gr_int,bl_int, or_int,ye_int, wh_int = (0,0,0,0,0,0)
+    COL_TUP =  (  (red_int, "RED", RED),  (gr_int, "GREEN", GR), (bl_int, "BL", BL),
+                    (or_int, "ORANGE", OR), (ye_int, "YELLOW", YE), (wh_int, "WHITE", WH) )
+    for n in COL_TUP:
+        print(f"comparing to {n[1]}")
+
+
+
+
 cub_img = os.path.join("/mnt/chromeos/MyFiles/Downloads/", "cubex.jpg")
 with Image.open(cub_img) as im:
     im.load()
@@ -16,6 +33,8 @@ with Image.open(cub_img) as im:
     for y in cube_smpling_pts:
         for x in cube_smpling_pts:
             print(f" location {x}, {y}")
-            print (im.getpixel((x,y)))
+            cube_smpl = im.getpixel((x,y))
+            print (cube_smpl)
+            get_color(cube_smpl)
 
 
