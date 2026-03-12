f288ee5ef04bc34fe720f385fae83a6bc879ea99
jprowak1 <jprowak@gmail.com>
2026-03-11 21:06:57 -0400

    in progress to read cube state file

--- file_sel_but.py
+++ file_sel_but.py
@@ -3,16 +3,26 @@ from tkinter import filedialog
 
 root = tk.Tk()
 root.title("Radiobutton")
-root.geometry("360x480")
+root.geometry("200x200")
 def load_cube_face():
-    filename = filedialog.askopenfilename(
+    fname = filedialog.askopenfilename(
         initialdir="/", # Start directory (use "/" for root or "C:/" on Windows)
         title="Select a file",
         filetypes=(
             ("Text files", "*.txt"),
             ("All files", "*.*")
-        ) # Filter file types
+        )
     )
+    # read the entire txt file
+    with open(fname), 'r') as myfile:
+        content = myfile.read()
+        # loop over the entire txt file, char at a time
+        for chr in content:
+            # check for UDRLFB chars
+            # check for 1,2,3 chars and space
+            # repear chars for number 2 or 3
+            # and push onto queue
+
 load_cf_but = tk.Button(root, text="Select cube face_file", command=load_cube_face)
 load_cf_but.pack()
 root.mainloop()
