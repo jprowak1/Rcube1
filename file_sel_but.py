de424188bc419eafef651c67d18a2f3b48521c84
jprowak1 <jprowak@gmail.com>
2026-03-15 20:52:07 -0400

    file_sel_but reads, parses and prints( later to push) the moves

--- file_sel_but.py
+++ file_sel_but.py
@@ -5,8 +5,10 @@ root = tk.Tk()
 root.title("Radiobutton")
 root.geometry("200x200")
 def load_cube_face():
+    moves = {"F", "B", "U", "D", "L", "R"}
+    nums = {"1", "2", "3"}
     fname = filedialog.askopenfilename(
-        initialdir="/", # Start directory (use "/" for root or "C:/" on Windows)
+        initialdir="/My files", # Start directory (use "/" for root or "C:/" on Windows)
         title="Select a file",
         filetypes=(
             ("Text files", "*.txt"),
@@ -14,13 +16,19 @@ def load_cube_face():
         )
     )
     # read the entire txt file
-    with open(fname), 'r') as myfile:
+    with open(fname, 'r') as myfile:
         content = myfile.read()
         # loop over the entire txt file, char at a time
         for chr in content:
             # check for UDRLFB chars
-            # check for 1,2,3 chars and space
-            # repear chars for number 2 or 3
+            if (chr in moves) :
+                prev_chr = chr
+                print(chr)
+            elif (chr in nums):
+                # repear chars for number 2 or 3
+                cntr =int(chr) -1
+                for _ in  range (cntr):
+                    print (prev_chr)
             # and push onto queue
 
 load_cf_but = tk.Button(root, text="Select cube face_file", command=load_cube_face)
